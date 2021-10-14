0c934509729459c3a2f90a0d0257b207a1d4f6fd
Saqib Nazir <saqib_9090@yahoo.com>
2021-10-15 00:43:05 +0100

    Single Product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './screens/HomePage';
-import SingleProduct from './screens/SingleProduct';
+import HomePage from './pages/HomePage';
+import SingleProduct from './pages/SingleProduct';
 
 const App = () => {
   return (
--- frontend/src/screens/SingleProduct.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SingleProduct = () => {
-  return <div>Product</div>;
-};
-
-export default SingleProduct;
