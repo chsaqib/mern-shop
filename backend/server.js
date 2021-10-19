e3017d6d3aa14857ea943e1320cfa6dd9d310322
Saqib Nazir <saqib_9090@yahoo.com>
2021-10-20 00:09:03 +0100

    backend routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
-const express = require("express")
+const express = require('express');
+const products = require('./data/products');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Api Is Running..');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen( PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
