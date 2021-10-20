d04d9e910c0dea34f31fdad49c17fde133fd7ad8
Saqib Nazir <saqib_9090@yahoo.com>
2021-10-20 01:16:29 +0100

    feching products from react

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
+dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
