328739d9f48c48ce65cef1842dddf33cfd5e024a
Saqib Nazir <saqib_9090@yahoo.com>
2021-10-21 21:21:20 +0100

    adding custom  error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 
 connectDB();
@@ -13,7 +13,9 @@ app.get('/', (req, res) => {
   res.send('Api Is Running..');
 });
 
-app.use('./api/products', productRoutes);
+app.use('/api/products', productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
